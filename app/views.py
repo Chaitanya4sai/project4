8f991f3bd7cb9a59bc678ebef678df3ace3cd2a0
Chaitanya4sai <saikumarpannuru261@gmail.com>
2023-11-23 11:16:27 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def daddy(request):
+    return HttpResponse('my pocket is empty.so i need money...')
